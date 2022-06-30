7e4e684b2d14934991843cc90d9270a1db70f687
David Aydin <david.aydin6@gmail.com>
2022-06-30 15:11:27 -0400

    override the __str__method

--- vehicle.py
+++ vehicle.py
@@ -6,20 +6,26 @@ class Vehicle():
             self.model = model
             self.running = False
     """
+
     def __init__(self, vin, make, model, running=False):
         self.vin = vin
         self.make = make
         self.model = model
         self.running = running
-
+    
     def start(self):
         self.running = True
     
     def stop(self):
         self.running = False
+    
+    def __str__(self):
+        return f'Vehicle VIN({self.vin}) is a {self.make} {self.model}'
 
 myCar = Vehicle('14', 'Dodge', 'Hellcat')
 print(myCar.vin, myCar.make, myCar.model)
+print(myCar)
+
 car = Vehicle('TS123', 'Tesla', 'Model S')
 print(car.running) # -> False
 car.start()
